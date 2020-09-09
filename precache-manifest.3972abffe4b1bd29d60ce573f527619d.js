d736fe8b86590639d881b3eb273c80e56a5c83d3
JeongWoo-Jo <whwjddn7364@naver.com>
2020-09-10 00:24:45 +0900

    Updates

---
+++ precache-manifest.3972abffe4b1bd29d60ce573f527619d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2a8676c62a56a6cc97a54cbf870e37b",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "21b403cbba864c4f9761",
+    "url": "/movie_app/static/css/main.a15c7332.chunk.css"
+  },
+  {
+    "revision": "2cb00f0b337cb1b1485d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21b403cbba864c4f9761",
+    "url": "/movie_app/static/js/main.264b7ee8.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
